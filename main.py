9c62f66400349f3ec4d7b8df5a1a3ce8255412ef
TaeFreeze <potaekingdom@hotmail.com>
2021-01-09 12:10:58 +0700

    Starter Project

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+import pandas as pd
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def result(res):
+    return {"result":res}
+
+@app.get("/")
+async def main():
+    return 'Hello World'
+
+@app.get("/List")
+async def List():
+    response = requests.get("https://taeapiplatform.herokuapp.com/ApiList")
+    return response
+
+
+if __name__ == '__main__':
+   uvicorn.run(app, host="0.0.0.0", port=80, debug=True)
