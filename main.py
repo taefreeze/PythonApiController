b2f4d4d292194ceb72400264550ba4da83558e07
TaeFreeze <potaekingdom@hotmail.com>
2021-01-15 17:44:04 +0700

    Signup json CORS

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
