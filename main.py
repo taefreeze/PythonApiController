78c7f7ecd2759e8914777ac315e93c72c215ff8c
TaeFreeze <potaekingdom@hotmail.com>
2021-01-18 17:47:33 +0700

    local

--- main.py
+++ main.py
@@ -19,53 +19,58 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 def result(res):
-    return {"result":res}
+    return {"result": res}
+
 
 @app.get("/")
 async def main():
     return 'Hello World'
+
 
 @app.get("/List")
 async def List():
     response = requests.get(ApiUrl.List).json()
     return response
 
+
 @app.post("/ApiSignUp")
-async def ApiSignUp(name_eng : str, name_th : str, api_url : str, param1 : str):
+async def ApiSignUp(name_eng: str, name_th: str, api_url: str, param1: str):
     data = {
-            'name_eng' : name_eng,
-            'name_th' : name_th, 
-            'api_url' : api_url, 
-            'param1' : param1
-            }
-    request = requests.post(url = ApiUrl.SignupThun, json=data)
+        'name_eng': name_eng,
+        'name_th': name_th,
+        'api_url': api_url,
+        'param1': param1
+    }
+    request = requests.post(url=ApiUrl.SignupThun, json=data)
     response = request.json()
-    status = {'status' : {'code' : request.status_code, 'reason' : request.reason}}
-    return response,status
+    status = {'status': {'code': request.status_code, 'reason': request.reason}}
+    return response, status
+
 
 @app.post("/ApiSignUpJson")
-async def ApiSignUpJson(Registers : Register):
+async def ApiSignUpJson(Registers: Register):
     name_eng = Registers.name_eng
     name_th = Registers.name_th
     api_url = Registers.api_url
     param1 = Registers.param1
     data = {
-            'name_eng' : name_eng,
-            'name_th' : name_th, 
-            'api_url' : api_url, 
-            'param1' : param1
-            }
-    request = requests.post(url = ApiUrl.SignupThun, json=data)
+        'name_eng': name_eng,
+        'name_th': name_th,
+        'api_url': api_url,
+        'param1': param1
+    }
+    request = requests.post(url=ApiUrl.SignupThun, json=data)
     response = request.json()
-    status = {'status' : {'code' : request.status_code, 'reason' : request.reason}}
-    return response,status
+    status = {'status': {'code': request.status_code, 'reason': request.reason}}
+    return response, status
+
 
 @app.post("/Update")
-async def Update(id : int,data : dict):
+async def Update(id: int, data: dict):
     return 0
-    
 
 
 if __name__ == '__main__':
-   uvicorn.run(app, host="0.0.0.0", port=80, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=80, debug=True)
